b8d500ab27204133fab0ebc0cc5223e7b97ed07f
mohitprajapati17 <mohitprajapati2444@gmail.com>
2026-06-07 23:12:01 +0530

    add profile

--- my-app/src/components/ProfileCard.tsx
+++ my-app/src/components/ProfileCard.tsx
@@ -1,11 +1,14 @@
 export default function ProfileCard({ image, name, bannerImage, description, location }: { image: any, name: string, bannerImage: string, description: string, location: string }) {
     return (
-        <div className="">
-            <img src={bannerImage} alt="" className="h-12  w-full rounded-t-2xl" />
-            {image}
-            <p>{name}</p>
-            <p>{description}</p>
-            <p>{location}</p>
+        <div className=" border-1 border-black  rounded-xl overflow-hidden">
+            <img src={bannerImage} alt="" className="h-12  w-full  " />
+            <div className="px-2">
+                {image}
+                <p className=" text-2xl font-bold">{name}</p>
+                <p className="text-xs text-gray-600">{description}</p>
+                <p className="text-xs text-gray-600">{location}</p>
+            </div>
+
         </div>
     );
 } 
--- my-app/src/components/ProfileSection.tsx
+++ my-app/src/components/ProfileSection.tsx
@@ -2,8 +2,34 @@ import ProfileCard from "./ProfileCard"
 import { UserRoundPen } from "lucide-react"
 export default function ProfileSection() {
     return (
-        <div className=" border-1 border-black  rounded-xl">
+        <div className="h-full border-1 border-black rounded-xl ">
             <ProfileCard image={<UserRoundPen />} name="mohit" bannerImage={"https://t3.ftcdn.net/jpg/03/16/91/28/360_F_316912806_RCeHVmUx5LuBMi7MKYTY5arkE4I0DcpU.jpg"} description="Front End Developer" location="Delhi" />
+            <div className=" mt-2  border-1 border-black rounded-xl">
+                <h1 className="text-sm">Gain exclusive site and content</h1>
+                <h1 className="text-blue-800"> Redeem premium   for free</h1>
+
+
+
+            </div>
+
+            <div className="mt-2  border-1 border-black rounded-xl ">
+                <div className="flex justify-between ">
+                    <p>Profile viewers</p>
+                    <p>123</p>
+
+
+                </div>
+
+                <div className="flex justify-between ">
+                    <p>Post impressions</p>
+                    <p>123</p>
+
+
+                </div>
+
+
+            </div>
         </div>
+
     );
 }
